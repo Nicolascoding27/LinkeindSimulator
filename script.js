1e36b439608ead1a79073d1d41baf88e69ec1fa6
Nicolascoding27 <nicolas.diaz27@hotmail.com>
2021-04-27 00:25:53 -0500

    everything done

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const openButton= document.querySelectorAll('[data-post-target]');
 const closeButton= document.querySelectorAll('[data-close-button]');
 const overlay= document.getElementById('overlay');
+
 openButton.forEach(button=>{
     button.addEventListener('click',()=>{
         //get the target from the data attribute in the html
@@ -20,9 +21,14 @@ function openPost(post){
     if(post==null)return
     post.classList.add('active')//add the class so we can se the write a post section
     overlay.classList.add('active')//take the variable overlay and add it the class active
+    const a =1 
 }
+console.log(a)
 function closePost(post){
     if(post==null)return
     post.classList.remove('active')//remove the class so we can't se the write a post section
     overlay.classList.remove('active')//take the variable overlay and remove the class so we dont see the overlay anymore
+}
+function close(element){
+    element.addEventListener('click',closePost(post))
 }
